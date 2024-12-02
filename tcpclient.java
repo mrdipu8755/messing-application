5e1ef0b42660b1644ff281e234ff5d2aec5a508a
Subhajit bhuyan <134502779+mrdipu8755@users.noreply.github.com>
2024-12-02 14:33:15 +0530

    Update tcpclient.java

--- tcpclient.java
+++ tcpclient.java
@@ -1,50 +1,43 @@
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Scanner;
-            public class tcpclient{
-                public static void main(String[] args )
-                {
-                    try{
-                        Socket c = new Socket("192.168.24.95",1111);
-                        System.out.println("Connected");
-                        Scanner scanner = new Scanner (System.in);
-                        DataInputStream din=new DataInputStream(c.getInputStream());
-                        DataOutputStream dout= new DataOutputStream(c.getOutputStream());
-                        while (true) 
-                        {
-                         System.out.println("Enter the Message:-");
-                         String cmsg =scanner.nextLine();
-                         dout.writeUTF(cmsg);
-                         if(cmsg.equals("e"
-                         		+ "xit"))
-                         {
-                            System.out.println("Quiting");
-                            din.close();
-                            dout.close();
-                            scanner.close();
-                            c.close();
-                            System.exit(0);
-                         } 
-                         String smsg =din.readUTF();
-                         System.out.println("Server:-"+smsg);
-                         if(smsg.equals("exit"))
-                         {
-                            System.out.println("Quiting!!!");
-                            din.close();
-                            dout.close();
-                            scanner.close();
-                            System.exit(0);
-                         }  
-                        }
-                    } catch(UnknownHostException e) {
-                       e.printStackTrace();
-                    } catch (IOException e){
-                    	e.printStackTrace();
 
+public class tcpclient {
+    public static void main(String[] args) {
+        try (Socket c = new Socket("192.168.24.95", 1111)) {
+            System.out.println("Connected to server!");
+
+            try (Scanner scanner = new Scanner(System.in);
+                 DataInputStream din = new DataInputStream(c.getInputStream());
+                 DataOutputStream dout = new DataOutputStream(c.getOutputStream())) {
+
+                while (true) {
+                    System.out.print("Enter the message: ");
+                    String cmsg = scanner.nextLine();
+                    dout.writeUTF(cmsg);
+
+                    if (cmsg.equalsIgnoreCase("exit")) {
+                        System.out.println("Disconnected from the server.");
+                        break;
+                    }
+
+                    String smsg = din.readUTF();
+                    System.out.println("Server: " + smsg);
+
+                    if (smsg.equalsIgnoreCase("exit")) {
+                        System.out.println("Server has terminated the connection.");
+                        break;
                     }
                 }
+            } catch (IOException e) {
+                System.out.println("Error while communicating with the server: " + e.getMessage());
             }
+
+        } catch (IOException e) {
+            System.out.println("Failed to connect to the server: " + e.getMessage());
+        }
+    }
+}
+
