b791d4dbb97dd4a319a4e2a47dd176b5f512d961
Subhajit bhuyan <134502779+mrdipu8755@users.noreply.github.com>
2024-12-02 14:34:30 +0530

    Update tcpserver.java

--- tcpserver.java
+++ tcpserver.java
@@ -8,43 +8,41 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class tcpserver {
-    public static void main (String [] args){
-        try{
-            ServerSocket ss = new ServerSocket(1111);
-            System.out.println("Waiting For Connection:-");
-            Socket s= ss.accept();
-            System.out.println("CONNECTED");
-            Scanner scanner = new Scanner(System.in);
-            DataInputStream din=new DataInputStream(s.getInputStream());
-            DataOutputStream dout=new DataOutputStream(s.getOutputStream());
-            while (true) 
-            {
-             String cmsg=din.readUTF();
-             System.out.println("Client:-"+cmsg);
-             if(cmsg.equals("exit"))
-             {
-                System.out.println("Quiting");
-                din.close();
-                dout.close();
-                scanner.close();
-                s.close();
-                System.exit(0);
-             }   
-             System.out.println("Enter message:-");
-             String smsg = scanner.nextLine();
-             dout.writeUTF(smsg);
-             if(smsg.equals("exit"))
-             {
-                System.out.println("Quiting!!!");
-                din.close();
-                dout.close();
-                scanner.close();
-                System.exit(0);
+    public static void main(String[] args) {
+        try (ServerSocket ss = new ServerSocket(1111)) {
+            System.out.println("Waiting for connection...");
+            Socket s = ss.accept();
+            System.out.println("Client connected!");
+
+            try (Scanner scanner = new Scanner(System.in);
+                 DataInputStream din = new DataInputStream(s.getInputStream());
+                 DataOutputStream dout = new DataOutputStream(s.getOutputStream())) {
+
+                while (true) {
+                    String cmsg = din.readUTF();
+                    System.out.println("Client: " + cmsg);
+                    if (cmsg.equalsIgnoreCase("exit")) {
+                        System.out.println("Client disconnected. Closing connection...");
+                        break;
+                    }
+
+                    System.out.print("Enter message: ");
+                    String smsg = scanner.nextLine();
+                    dout.writeUTF(smsg);
+
+                    if (smsg.equalsIgnoreCase("exit")) {
+                        System.out.println("Server shutting down...");
+                        break;
+                    }
                 }
-            }            
+            } catch (IOException e) {
+                System.out.println("Error while communicating with the client: " + e.getMessage());
+            }
+
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Server failed to start: " + e.getMessage());
         }
     }
 }
+
 
